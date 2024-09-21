c255324feb3b47705418402979bd0970a8308dbc
Recep Pazarlı <receppazarli99@hotmail.com>
2024-09-21 13:16:36 +0300

    Add project files.

---
+++ EFP-2/DataAccessLayer/ManagementContext.cs
@@ -0,0 +1,18 @@
+﻿using EFP_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EFP_2.DataAccessLayer
+{
+    public class ManagementContext : DbContext
+    {
+        public ManagementContext() : base("ManagementDatabase") { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+    }
+}
---
+++ EFP-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFP_2.MvcApplication" Language="C#" %>
